93b682e1f34af2ef881361cd34cec8792dd76e74
Denis <voland2114@gmail.com>
2019-11-29 16:41:05 +0700

    add products

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,11 @@
-function hello(){
-   console.log('hello');
-}
-hello();
+
+
+$(function(){
+   $('.products__slider').slick({
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      dots: true,
+    });
+ });
 
