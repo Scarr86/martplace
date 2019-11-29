b29aa72f4d65a163b27010e50766cf26862e4385
DEN <voland2114@gmail.com>
2019-11-30 00:43:34 +0700

    finish products section

--- app/js/main.js
+++ app/js/main.js
@@ -1,15 +1,22 @@
 
 
-$(function(){
-   $('.products__slider').slick({
+$(function () {
+   $('.slider-products').slick({
       infinite: true,
       slidesToShow: 1,
       slidesToScroll: 1,
       // dots: true,
       // arrows:false,
       // appendArrows: $('.products__title-control'),
-      prevArrow: $('.button-left'),
-      nextArrow: $('.button-right'),
-    });
- });
+      prevArrow: $('.title__btn-left'),
+      nextArrow: $('.title__btn-right'),
+   });
+
+   $(".rate-star").rateYo({
+      rating: 3.6,
+      starWidth: "15px",
+      readOnly: true,
+      ratedFill: "#ffc000"
+   });
+});
 
